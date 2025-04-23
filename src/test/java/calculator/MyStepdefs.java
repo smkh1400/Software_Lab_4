374ab7229b512275802b66ab9de2529894b0e00e
smkh1400 <mz2012kh82@gmail.com>
2025-04-23 21:54:49 +0330

    Configured maven and its dependencies, Implemented normal scenario

---
+++ src/test/java/calculator/MyStepdefs.java
@@ -0,0 +1,34 @@
+package calculator;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class MyStepdefs {
+    private Calculator calculator;
+    private int value1;
+    private int value2;
+    private int result;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Given("^Two input values, (\\d+) and (\\d+)$")
+    public void twoInputValuesAnd(int arg0, int arg1){
+        value1 = arg0;
+        value2 = arg1;
+    };
+    @When("^I add the two values$")
+    public void iAddTheTwoValues() {
+        result = calculator.add(value1, value2);
+        System.out.println(result);
+    }
+    @Then("^I expect the result (\\d+)$")
+    public void iExpectTheResult(int arg0) {
+        Assert.assertEquals(arg0, result);
+    }
+
+}
